55060d9d8d03d845d787b73c3227d28f9d707082
faroliwi <abad.simon.adrian.smr@gmail.com>
2021-11-18 15:56:24 +0100

    18/11/21

--- src/app/views/home.php
+++ src/app/views/home.php
@@ -3,10 +3,11 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="../../assets/css/style-home.css" />
         <title>Home</title>
     </head>
     <body>
-        <h1>Corte & Estilo</h1>
+        <h1>Corte y Estilo</h1>
         <h2>Salón de belleza</h2>
         <!-- /clase/metodo -->
         <a href="/login">Acceso administradora</a>
