e1ea15c29da2c12cc54cb503f420cc50619122e8
faroliwi <abad.simon.adrian.smr@gmail.com>
2021-11-22 20:25:14 +0100

    22/11/21 - Segundo push del dia

--- src/app/views/homeAdmin.php
+++ src/app/views/homeAdmin.php
@@ -3,9 +3,11 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <title>Home</title>
     </head>
     <body>
+        <?php require "app/views/admin/workers/parts/header.php"?>
         <h1>Home Administrador</h1>
         <br>
         <form method="POST" action="/workers">
