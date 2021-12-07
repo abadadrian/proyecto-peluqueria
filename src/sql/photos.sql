e7ef9fc19e95ddb0cd1074d9de4593748b318c9c
faroliwi <abad.simon.adrian.smr@gmail.com>
2021-12-07 15:59:14 +0100

    7/12/21 - Cambios en photos.sql

--- src/sql/photos.sql
+++ src/sql/photos.sql
@@ -2,6 +2,6 @@ USE `pelu`;
 CREATE TABLE IF NOT EXISTS `photos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
-  `photo` varchar(200) NOT NULL,
+  `path` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
