ba9da5f2e30db8dc805173c494d629c7ec9385eb
faroliwi <abad.simon.adrian.smr@gmail.com>
2021-12-12 12:28:37 +0100

    12/12/21 - Nav finalizado

--- src/app/views/admin/parts/header.php
+++ src/app/views/admin/parts/header.php
@@ -1,15 +1,11 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Corte y Estilo</a>
+  <a class="navbar-brand ms-1" href="/admin">Corte y Estilo</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-    
-
-    
-    <ul class="navbar-nav">
-      
+    <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="/workers">Trabajadores</a>
       </li>
@@ -17,8 +13,13 @@
         <a class="nav-link" href="/services">Servicios</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="/photos/index" >Fotos</a>
+        <a class="nav-link" href="/photos/index">Fotos</a>
       </li>
     </ul>
+  </div class="container">
+    <form method="POST" action="/login/logout">
+      <button type="submit" class="btn btn-dark ">Cerrar sesión</button>
+    </form>
+  </div>
   </div>
 </nav>
--- src/app/views/homeAdmin.php
+++ src/app/views/homeAdmin.php
@@ -6,26 +6,13 @@
 </head>
 
 <body>
+    <?php require "app/views/admin/parts/header.php" ?>
     <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item active mt-3">Home Admin</li>
         </ol>
     </nav>
     <h1 class="mb-4">Home Administrador</h1>
-    <form method="POST" action="/workers" class="mb-4">
-        <button type="submit" class="btn btn-dark">Trabajadores</button>
-    </form>
-    <form method="POST" action="/services" class="mb-4">
-        <button type="submit" class="btn btn-dark">Servicios</button>
-    </form>
-    <form method="POST" action="/photos/index" class="mb-4">
-        <button type="submit" class="btn btn-dark">Añadir foto</button>
-    </form>
-    <form method="POST" action="/login/logout">
-        <button type="submit" class="btn btn-dark">Cerrar sesión</button>
-    </form>
-    
-
     <?php require "app/views/admin/parts/footer.php" ?>
 </body>
 <?php require "app/views/admin/parts/scripts.php" ?>
